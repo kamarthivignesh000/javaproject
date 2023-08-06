9f0fcea70ba336b22ccd82db58c6efd43cda0acd
Vignesh <Vignesh@DESKTOP-1NF3S15>
2023-08-06 22:57:27 +0530

    Updated Project

--- demoproject/src/com/demoproject/FirstProject.java
+++ demoproject/src/com/demoproject/FirstProject.java
@@ -4,7 +4,6 @@ public class FirstProject {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World...!");
-		System.out.println("Hello World Again...!");
-		System.out.println("Hello World Again Again...!");
+		System.out.println("Hello Super World Again...!");
 	}
 }
